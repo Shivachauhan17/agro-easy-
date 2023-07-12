dca40af056e575e8c4c27214e1161655149e282a
Shiva chauhan <103742975+Shivachauhan17@users.noreply.github.com>
2023-07-12 20:13:14 +0530

    Update newsPage.js
    successfuly fetched news

--- assets/newsPage/newsPage.js
+++ assets/newsPage/newsPage.js
@@ -8,30 +8,54 @@ fetch(req)
         console.log(data)
         console.log(data['totalResults'])
         for(let i=3;i<data['totalResults'];i++){
-            if(i<=11){
+            if(i<=10){
                 let extra=i-2
                 extra=String(extra)
                 hid="sech1"+extra
                 pid="secp"+extra
                 imgid="img"+extra
                 if(data['articles'][i]['urlToImage']!=null){
-                //document.getElementById(imgid).src=data['articles'][i]['urlToImage']
+                document.getElementById(imgid).src=data['articles'][i]['urlToImage']
                 }
-                //document.getElementById(pid).innerText=data['articles'][i]['content']
-                //document.getElementById(hid).innerText=data['articles'][i]['title']
+                else{
+                    document.getElementById(imgid).src ='images.jpeg'
+                }
+                document.getElementById(pid).innerText=data['articles'][i]['content']
+                document.getElementById(hid).innerText=data['articles'][i]['title']
             }
             else{
-                const node=document.getElementsByClassName("continues")
-                const clone=node[0].cloneNode(true)
-                clone.firstChild.src=data['articles'][i]['urlToImage']
-                clone.lastChild.firstChild.innerHTML=data['articles'][i]['title']
-                clone.lastChild.lastChild.innerHTML=data['articles'][i]['content']
-                console.log(clone)
-                document.getElementsByName('body').appendChild(clone)
+                let section=document.createElement('section')
+                section.setAttribute("id","mainsec_continue")    
+                let img=document.createElement('img')
+                console.log(data)
+                if(data['articles'][i]['urlToImage']!=null && data['articles'][i]['urlToImage']!=undefined ){
+                    
+                    img.setAttribute("src",data['articles'][i]['urlToImage'])  
+                }
+                else{
+                    img.setAttribute("src",'images.jpeg')  
+                } 
+                img.setAttribute("class","continues")
+                let subsection=document.createElement('section')
+                subsection.setAttribute("class","sec_continue")
+                let h=document.createElement('h2')
+                h.textContent=data['articles'][i]['title']
+                let p=document.createElement('p')
+                p.textContent=data['articles'][i]['content']
+                subsection.append(h)
+                subsection.append(p)
+
+
+                section.append(img)
+                section.append(subsection)
+
+                let parent=document.getElementById("body")
+                let following=document.getElementById("continues")
+                console.log(section)
+                parent.insertBefore(section,following)
+
             }
         }
 
     })
     
-
-
