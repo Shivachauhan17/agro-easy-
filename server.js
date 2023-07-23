8221225ce954adf96aaff36e5d75c789f22a6cac
Shivachauhan17 <shivanbd2019@gmail.com>
2023-07-23 22:21:31 +0530

    completed node server

--- server.js
+++ server.js
@@ -18,68 +18,127 @@ const server = http.createServer((req,res) => {
         })
     }
     else if(page == '/assets/css/header.css'){
-        fs.readFile('/assets/css/header.css', (error,data) => {
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/header.css', (error,data) => {
             res.writeHead(200, {'Content-Type' : 'text/css'})
             res.write(data)
             res.end()
         })
     }
     else if(page == '/assets/css/main.css'){
-        fs.readFile('/assets/css/main.css', (error,data) => {
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/main.css', (error,data) => {
             res.writeHead(200, {'Content-Type' : 'text/css'})
             res.write(data)
             res.end()
         })
     }
     else if(page == '/assets/css/footer.css'){
-        fs.readFile('/assets/css/footer.css', (error,data) => {
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/footer.css', (error,data) => {
             res.writeHead(200, {'Content-Type' : 'text/css'})
             res.write(data)
             res.end()
         })
     }
     else if(page == '/assets/img/logo.webp'){
-        fs.readFile('/assets/img/logo.webp', (error,data) => {
-            res.writeHead(200, {'Content-Type' : 'image/webp'})
-            res.write(data)
-            res.end()
-        })
-    }
-    else if(page == '/assets/monitor/monitor.html'){
-        fs.readFile('/assets/monitor/monitor.html', (error, data) => {
-            res.writeHead(200, {'Content-Type' : 'text/html'})
-            res.write(data)
-            res.end()
-        })
-    }
-    else if(page == '/assets/newsPage/newsPage.html'){
-        fs.readFile('/assets/newsPage/newsPage.html', (error,data) => {
-            res.writeHead(200, {'Content-Type' : 'text/html'} )
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/img/logo.webp', (error,data) => {
+            res.writeHead(200, {'Content-Type' :  'image/webp'})
             res.write(data)
             res.end()
         })
     }
     else if(page == '/assets/css/linkedinLogo.png'){
-        fs.readFile('/assets/css/linkedinLogo.png', (error,data) => {
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/linkedinLogo.png', (error,data) => {
             res.writeHead(200, {'Content-Type' : 'image/png'})
             res.write(data)
             res.end()
         })
     }
     else if(page == '/assets/css/twitterLogo.jpeg'){
-        fs.readFile('/assets/css/twitterLogo.jpeg', (error, data) => {
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/twitterLogo.jpeg', (error, data) => {
             res.writeHead(200, {'Content-Type' : 'image/jpeg'})
             res.write(data)
             res.end()
         })
     }
-    else if(page == '/assets/newsPage/newsPage.js'){
-        fs.readFile('/assets/newsPage/newsPage.js', (error,data) => {
-            res.writeHead(200, {'Content-Type' : 'text/javascript'} )
+    else if(page == '/assets/css/bodyBackground.jpg'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/css/bodyBackground.jpg', (error, data) => {
+            res.writeHead(200, {'Content-Type' : 'image/jpg'})
             res.write(data)
             res.end()
         })
     }
+
+
+
+
+    else if(page == '/assets/monitor/monitor.html'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/monitor/monitor.html', (error, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/assets/monitor/monitor.css'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/monitor/monitor.css', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'text/css'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/assets/img/dummy_monitor_img.png'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/img/dummy_monitor_img.png', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'image/png'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/assets/img/dummy_person_dp.jpeg'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/img/dummy_person_dp.jpeg', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'image/jpeg'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/index.html'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/index.html', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }
+    
+
+
+
+
+    else if(page == '/assets/newsPage/newsPage.html'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/newsPage/newsPage.html', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'} )
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/assets/newsPage/newPage.css'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/newsPage/newPage.css', (error,data) => {
+            res.writeHead(200, {'Content-Type' : 'text/css'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(page == '/assets/newspage/newsPage.js'){
+        fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/newsPage/newsPage.js', (error, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/javascript'})
+            res.write(data)
+            res.end()
+        })
+    }
+
+    // else if(page == '/assets/newsPage/newsPage.js'){
+    //     fs.readFile('/home/shiva1974/Desktop/farmer-crop-health-assist-website/assets/newsPage/newsPage.js', (error,data) => {
+    //         res.writeHead(200, {'Content-Type' : 'text/javascript'} )
+    //         res.write(data)
+    //         res.end()
+    //     })
+    // }
 })
 
 server.listen(8000)
