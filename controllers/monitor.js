a7f2bacbfd8d2dc504cf66f8aca650db85a383f1
Shivachauhan17 <shivanbd2019@gmail.com>
2023-08-06 22:04:44 +0530

    trying to add model

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,5 +1,15 @@
+const farmerData=require('../model/farmer-data')
 module.exports={
     getMonitorPage:async(req,res)=>{
-        res.render('monitor.ejs')
+        try{
+            const farmer_data=farmerData.find()
+            console.log('****************************************************************')
+            console.log(farmer_data[0])
+            res.render('monitor.ejs',{info:farmer_data})
+        }
+        catch(error){
+            console.log(error)
+        }
     }
+    
 }
--- routes/monitor.js
+++ routes/monitor.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router=express.Router()
 const monitorController=require('../controllers/monitor')
-router.get('/monitor',monitorController.getMonitorPage)
+router.get('/',monitorController.getMonitorPage)
+//router.get('/addComment',monitorController.addComment)
+//router.get('/deleteComment',monitorController.deleteComment)
 
 module.exports=router
