4e08ddac463314309c5a4bf715e2416e84ca4a3b
Shivachauhan17 <shivanbd2019@gmail.com>
2023-08-12 22:24:37 +0530

    still adding auth

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(logger('dev'))
 
 app.use(
     session({
